0fcde8e60ad5ad2495e7362c8a326576092b3d69
Ajaykumarch15 <150650974+Ajaykumarch15@users.noreply.github.com>
2025-11-13 22:05:47 +0530

    Change import path for projects data file

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { projects } from "../data/projects";
+import { projects } from "../data/projects.js";
 import { motion } from "framer-motion";
 import { useEffect } from "react";
 
